66d035f8e6ac934e82a841b22406e9dc60b20bd9
Lindsay Strydom <lindsay.strydom@asos.com>
2022-07-04 10:52:07 +0100

    updating fonts and images, getting map working, changing colours

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 52.07206135791627, lng:-0.7595706770551085};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
